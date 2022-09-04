ee9b07f6704c9f9ca6bfd21e98922412afd8fa29
BidibulleKing <remy.delepaule@gmail.com>
2022-09-04 20:41:33 +0200

    Exercice terminé

--- php/ensalle.php
+++ php/ensalle.php
@@ -1,3 +1,38 @@
+<!-- 
+  #1 - Afficher la première salle 1️⃣
+dans la page "en salle", ajouter un titre "Salles" après la liste des films
+puis ajouter une balise <ul>
+puis à l'intérieur ajouter une balise <li>
+dans cette balise <li>, ouvrir l'interpréteur PHP pour qu'il affiche "Athéna"
+en utilisant le tableau $rooms 😉
+#2 - Afficher la deuxième salle 2️⃣
+ajouter une seconde balise <li>
+dans cette balise <li>, ouvrir l'interpréteur PHP pour qu'il affiche "Dyonisos"
+en utilisant le tableau $rooms 😉
+#3 - Analyser 🔎
+les 2 lignes de code pour les deux <li> sont quasiment similaires
+trouver LA partie changeante qui sera à remplacer par une variable
+cette variable sera donc la variable qui va "boucler", changer de valeur dans la boucle
+Etapes intermédiaires en cas de blocage pour écrire la boucle de l'étape #4
+#4 - Boucler ➿
+Rappels
+
+les étapes précédentes nous ont permis de cibler le code à répéter dans une boucle
+$rooms est le tableau contenant la liste des salles à afficher
+Todo
+
+on va remplacer les 2 affichages "<li>" par une boucle
+écrire une boucle (avec while ou for) permettant de changer la valeur de la variable "trouvée" en étape #3
+initialiser cette variable avec la première valeur (permettant d'avoir la première salle)
+boucler "tant que" la variable n'a pas dépassé la dernière valeur (permettant d'avoir la dernière salle)
+entre chaque "boucle"/"iteration" ajouter 1 à la variable
+entre les accolades de cette boucle { ... }, le code va se répéter, s'exécuter à chaque "boucle"/"iteration"
+y placer l'affichage d'un <li> avec le nom de salle
+ce qu'on a fait en étape 3
+si besoin, commencer sans les <li>, et les rajouter ensuite
+toutes les salles du tableau $rooms doivent s'afficher
+ -->
+
 <?php include("../templates/header.php"); ?>
 <?php
 
@@ -19,12 +54,18 @@
   <main class="main-container">
     <section>
       <h2 class="page__title">Films en salle</h2>
+      <h3 class="page__subtitle">Salles</h3>
+      <ul>
+        <?php 
+        $i = 0;
+        while($i < count($rooms)) {
+          echo "<li>$rooms[$i]</li>";
+          $i += 1;
+        } 
+        ?>
+      </ul>
     </section>
   </main>
-
-  <?php
-    // ici je peux faire des echo ... 
-  ?>
 
   <footer class="footer">
     Cinéma Rodia - 42, avenue Foch, Haut-Cloques &copy; Tous droits réservés
--- php/tarifs.php
+++ php/tarifs.php
@@ -1,63 +1,96 @@
+<?php
+// [...]
+$extras = [
+  [
+    'Popcorn',
+    'L',
+    2.9
+  ],
+  [
+    'Popcorn',
+    'XL',
+    4
+  ],
+  [
+    'Chips',
+    '50g',
+    2.5
+  ],
+  [
+    'M&M\'s',
+    '100g',
+    4
+  ],
+  [
+    'Soda',
+    '33cl',
+    3.2
+  ],
+  [
+    'Evian',
+    '33cl',
+    3
+  ]
+];
+// L'instruction include me permet d'inclure du contenu qui se situe dans
+// un autre fichier PHP
+include("../templates/header.php");
+?>
+<main class="main-container">
+  <section>
+    <h2 class="page__title">Tarifs</h2>
+    <div class="prices__lists">
+
+      <div class="prices__list">
+        <h3 class="prices__list-title">
+          A l'unité
+        </h3>
+        <ul>
+          <li class="prices__item">
+            <span class="prices__item-desc">Tarif Plein</span> <span class="prices__item-value">8,30 &euro;</span>
+          </li>
+          <li class="prices__item">
+            <span class="prices__item-desc">Tarif Réduit</span> <span class="prices__item-value">6,80 &euro;</span>
+          </li>
+          <li class="prices__item">
+            <span class="prices__item-desc">Tarif Enfant</span> <span class="prices__item-value">4,50 &euro;</span>
+          </li>
+          <li class="prices__item">
+            <span class="prices__item-desc">Supplément 3D</span> <span class="prices__item-value">1,00 &euro;</span>
+          </li>
+        </ul>
+      </div>
+
+      <div class="prices__list">
+        <h3 class="prices__list-title">
+          Abonnement
+        </h3>
+        <ul>
+          <li class="prices__item">
+            <span class="prices__item-desc">5 places</span> <span class="prices__item-value">-10%</span>
+          </li>
+          <li class="prices__item">
+            <span class="prices__item-desc">5 places -25ans </span> <span class="prices__item-value">-20%</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="prices__description">
+      <p class="prices__description-line">
+        <strong class="prices__description-name">Tarif réduit</strong> pour les personnes de + de 60 ans et de moins de 16 ans
+      </p>
+      <p class="prices__description-line">
+        <strong class="prices__description-name">Tarif enfant</strong> pour les - de 14 ans
+      </p>
+    </div>
+  </section>
+
   <?php
-    // L'instruction include me permet d'inclure du contenu qui se situe dans
-    // un autre fichier PHP
-    include("../templates/header.php");
-  ?>
-  <main class="main-container">
-    <section>
-      <h2 class="page__title">Tarifs</h2>
-      <div class="prices__lists">
+  // Afficher le tarif pour l'âge du capitaine
+  // On se dit que l'âge du capitaine est de 43 ans.
 
-        <div class="prices__list">
-          <h3 class="prices__list-title">
-            A l'unité
-          </h3>
-          <ul>
-            <li class="prices__item">
-              <span class="prices__item-desc">Tarif Plein</span> <span class="prices__item-value">8,30 &euro;</span>
-            </li>
-            <li class="prices__item">
-              <span class="prices__item-desc">Tarif Réduit</span> <span class="prices__item-value">6,80 &euro;</span>
-            </li>
-            <li class="prices__item">
-              <span class="prices__item-desc">Tarif Enfant</span> <span class="prices__item-value">4,50 &euro;</span>
-            </li>
-            <li class="prices__item">
-              <span class="prices__item-desc">Supplément 3D</span> <span class="prices__item-value">1,00 &euro;</span>
-            </li>
-          </ul>
-        </div>
-
-        <div class="prices__list">
-          <h3 class="prices__list-title">
-            Abonnement
-          </h3>
-          <ul>
-            <li class="prices__item">
-              <span class="prices__item-desc">5 places</span> <span class="prices__item-value">-10%</span>
-            </li>
-            <li class="prices__item">
-              <span class="prices__item-desc">5 places -25ans </span> <span class="prices__item-value">-20%</span>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="prices__description">
-        <p class="prices__description-line">
-          <strong class="prices__description-name">Tarif réduit</strong> pour les personnes de + de 60 ans et de moins de 16 ans
-        </p>
-        <p class="prices__description-line">
-          <strong class="prices__description-name">Tarif enfant</strong> pour les - de 14 ans
-        </p>
-      </div>
-    </section>
-
-    <?php
-      // Afficher le tarif pour l'âge du capitaine
-      // On se dit que l'âge du capitaine est de 43 ans.
-
-      /*
+  /*
         Tarif Plein 8,30 €
         Tarif Réduit 6,80 €
         Tarif Enfant 4,50 €
@@ -74,37 +107,37 @@
         
       */
 
-      $age = 43;
+  $age = 43;
 
-      $tarifPlein = 8.30;
-      $tarifReduit = 6.80;
-      $tarifEnfant = 4.50;
+  $tarifPlein = 8.30;
+  $tarifReduit = 6.80;
+  $tarifEnfant = 4.50;
 
-      // Par défaut
-      $tarifCapitaine = $tarifPlein;
+  // Par défaut
+  $tarifCapitaine = $tarifPlein;
 
-      // Conditions
-      if ($age > 60 || $age < 16) {
-        $tarifCapitaine = $tarifReduit;
-      }
+  // Conditions
+  if ($age > 60 || $age < 16) {
+    $tarifCapitaine = $tarifReduit;
+  }
 
-      // Si l'âge est < 14, ça remplace la valeur définie plus haut !
-      // Du coup, je rempli les deux conditions ! Magie
-      if ($age < 14) {
-        $tarifCapitaine = $tarifEnfant;
-      }
+  // Si l'âge est < 14, ça remplace la valeur définie plus haut !
+  // Du coup, je rempli les deux conditions ! Magie
+  if ($age < 14) {
+    $tarifCapitaine = $tarifEnfant;
+  }
 
-      // J'utilise les doubles quotes pour y mettre une phrase composé
-      // du nom de ma variable avec son contenu (concaténation)
-      $chaineAge = "Age : $age";
-      $chaineAge = 'Age :'.$age;
+  // J'utilise les doubles quotes pour y mettre une phrase composé
+  // du nom de ma variable avec son contenu (concaténation)
+  $chaineAge = "Age : $age";
+  $chaineAge = 'Age :' . $age;
 
-      //var_dump($chaineAge);
-      //var_dump($tarifCapitaine);
+  //var_dump($chaineAge);
+  //var_dump($tarifCapitaine);
 
-      // AFFICHER UNE VARIABLE
-      // https://www.php.net/manual/fr/language.basic-syntax.phptags.php
-      /*
+  // AFFICHER UNE VARIABLE
+  // https://www.php.net/manual/fr/language.basic-syntax.phptags.php
+  /*
         DEUX ECRITURES EQUIVALENTES
         sucre syntaxique (short tag / PHP)
 
@@ -112,48 +145,68 @@
         - <?= $age ?>
       */
 
-    ?>
+  ?>
 
-    <section>
-      <h2 class="page__title">Tarif du capitaine</h2>
+  <section>
+    <h2 class="page__title">Tarif du capitaine</h2>
 
+    <div class="prices__list">
+      <h3 class="prices__list-title">
+        <span class="prices__item-desc">Age</span> <span class="prices__item-value">Tarif unitaire</span>
+      </h3>
+      <ul>
+        <li class="prices__item">
+          <span class="prices__item-desc"><?= $age ?> ans</span> <span class="prices__item-value"><?php echo $tarifCapitaine; ?> &euro;</span>
+        </li>
+      </ul>
+    </div>
+
+  </section>
+
+  <section>
+    <h2 class="page__title">Tarif âge par âge</h2>
+
+    <div class="prices">
       <div class="prices__list">
         <h3 class="prices__list-title">
           <span class="prices__item-desc">Age</span> <span class="prices__item-value">Tarif unitaire</span>
         </h3>
         <ul>
           <li class="prices__item">
-            <span class="prices__item-desc"><?= $age ?> ans</span> <span class="prices__item-value"><?php echo $tarifCapitaine; ?> &euro;</span>
+            <span class="prices__item-desc">.. ans</span> <span class="prices__item-value">.... &euro;</span>
+          </li>
+          <li class="prices__item">
+            <span class="prices__item-desc">.. ans</span> <span class="prices__item-value">.... &euro;</span>
           </li>
         </ul>
       </div>
+    </div>
 
-    </section>
+  </section>
 
-    <section>
-      <h2 class="page__title">Tarif âge par âge</h2>
-      
-      <div class="prices">
-        <div class="prices__list">
-          <h3 class="prices__list-title">
-            <span class="prices__item-desc">Age</span> <span class="prices__item-value">Tarif unitaire</span>
-          </h3>
-          <ul>
+  <section>
+    <h2 class="page__title">Consommations</h2>
+
+    <div class="prices">
+      <div class="prices__list">
+        <h3 class="prices__list-title">
+          <span class="prices__item-desc">Article</span> <span class="prices__item-value">Tarif unitaire</span>
+        </h3>
+        <ul>
+          <?php for ($i = 0; $i < count($extras); $i++) : ?>
             <li class="prices__item">
-              <span class="prices__item-desc">.. ans</span> <span class="prices__item-value">.... &euro;</span>
+              <span class="prices__item-desc"><?= $extras[$i][0] . ' (' . $extras[$i][1] . ')'; ?></span> <span class="prices__item-value"><?= $extras[$i][2] ?> &euro;</span>
             </li>
-            <li class="prices__item">
-              <span class="prices__item-desc">.. ans</span> <span class="prices__item-value">.... &euro;</span>
-            </li>
-          </ul>
-        </div>
+          <?php endfor; ?>
+        </ul>
       </div>
+    </div>
+  </section>
 
-    </section>
-
-  </main>
-  <footer class="footer">
-    Cinéma Rodia - 42, avenue Foch, Haut-Cloques &copy; Tous droits réservés
-  </footer>
+</main>
+<footer class="footer">
+  Cinéma Rodia - 42, avenue Foch, Haut-Cloques &copy; Tous droits réservés
+</footer>
 </body>
+
 </html>
